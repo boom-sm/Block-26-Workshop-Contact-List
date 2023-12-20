e80b065ed090d15b157df42ef9f71cc53b7b568d
boom-sm <santiparp1806754@gmail.com>
2023-12-20 02:54:58 -0500

    not done yet

---
+++ contact list/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css';
+import './components/Contactlist';
+import ContactList from './components/Contactlist';
+
+function App() {
+
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
+
+
+
+export default App;
+  
